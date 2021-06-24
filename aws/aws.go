8c0e0f1dd8b9cce6fa5d1031c9b3bd4dd29c0b8d
Brett Lentz <brett.lentz@tamr.com>
2021-06-24 09:33:14 -0400

    fork cloud-haunter, update references

--- aws/aws.go
+++ aws/aws.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/blentz/cloud-haunter/utils"
 	"net/http"
 	"os"
 	"strings"
@@ -24,8 +26,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/rds"
-	ctx "github.com/hortonworks/cloud-haunter/context"
-	"github.com/hortonworks/cloud-haunter/types"
+	ctx "github.com/blentz/cloud-haunter/context"
+	"github.com/blentz/cloud-haunter/types"
 	log "github.com/sirupsen/logrus"
 )
 
